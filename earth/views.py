97b1bc629e290ae93cc218a53af216013f959f42
Piyush046 <piyushpandey046@gmail.com>
2024-01-28 03:46:27 +0530

    first commit

---
+++ earth/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+ 
+ r=  """<div style="width: 200px; height: 100px; background-color: #27ae60; color: #fff; text-align: center; line-height: 100px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+    Hello, World!
+  </div>"""
+  
+ return HttpResponse(r)
