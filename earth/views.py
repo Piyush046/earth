e70f8c813b1bf54ada2f581dd19aeb7e20504a82
Piyush046 <piyushpandey046@gmail.com>
2024-01-29 04:00:09 +0530

    v7

--- earth/views.py
+++ earth/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
  
  r=  """<div style="width: 200px; height: 100px; background-color: #27ae60; color: #fff; text-align: center; line-height: 100px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-    Hello, World!
+    Hello, piyush!
   </div>"""
   
  return HttpResponse(r)
